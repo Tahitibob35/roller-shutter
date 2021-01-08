3e068b951d0c6775892f63261707965d9858fc3b
olivier <olivier.berthomme@gmail.com>
2021-01-08 11:54:56 +0100

    Olivier remote names

--- src/vars-olivier.h
+++ src/vars-olivier.h
@@ -1,4 +1,19 @@
 #ifndef VARS_H_
 #define VARS_H_
 
+#define REMOTES_COUNT 10
+
+const char *remote_name[REMOTES_COUNT] = { 
+  (char *)"Cuisine", 
+  (char *)"Baie vitr&eacute;e",
+  (char *)"Buffet",
+  (char *)"Porte fen&ecirc;tre",
+  (char *)"Bureau",
+  (char *)"Chambre 1",
+  (char *)"Chambre 2",
+  (char *)"Chambre 3",
+  (char *)"Tout bas",
+  (char *)"Tout haut",
+};
+
 #endif /* VARS_H_ */
